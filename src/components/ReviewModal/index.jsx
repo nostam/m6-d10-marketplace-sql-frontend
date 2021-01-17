e82ba6b989c54f2a9acd15790871368f2eaaeba1
nostam <matsonho@gmail.com>
2021-01-17 23:57:36 +0100

    add CRUD review modal and update product detail page

---
+++ src/components/ReviewModal/index.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import { Form, Button, Modal } from "react-bootstrap";
+export default class ReviewModal extends Component {
+  state = { review: {} };
+  url = process.env.REACT_APP_API_URL + "/products";
+  handleSubmitReview = async (option) => {
+    const method = option === "DELETE" ? "DELETE" : this.props.method;
+    try {
+      let res = await fetch(
+        `${this.url}/${this.props.product._id}/reviews${
+          method !== "POST" ? "/" + this.props.reviewId : ""
+        }`,
+        {
+          method: method,
+          headers: {
+            "content-type": "application/json",
+            origin: process.env.REACT_APP_ORIGIN,
+          },
+          body: JSON.stringify(this.state.review),
+        }
+      );
+      if (!res.ok) throw new Error();
+      this.props.onHide();
+      this.props.refetch();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getReview = async () => {
+    try {
+      let res = await fetch(
+        `${this.url}/${this.props.product._id}/reviews/${this.props.reviewId}`
+      );
+      if (!res.ok) throw new Error();
+      const data = await res.json();
+      this.setState({ review: data });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  handleReview = (e) => {
+    this.setState({
+      review: { ...this.state.review, [e.target.name]: e.target.value },
+    });
+    console.log(this.state.review);
+  };
+  componentDidMount = () => {
+    if (this.props.method === "PUT") this.getReview();
+  };
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.reviewId !== this.props.reviewId) this.getReview();
+  };
+  render() {
+    const { method, show, onHide, product } = this.props;
+    return (
+      <Modal show={show} onHide={onHide}>
+        <Modal.Header closeButton={show}>
+          <Modal.Title>
+            Review for {product.name} {this.props.reviewId}
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Rating</Form.Label>
+              <Form.Control
+                as="select"
+                name="rating"
+                value={this.state.review.rating}
+                onChange={(e) => this.handleReview(e)}
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Review</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="comment"
+                value={this.state.review.comment}
+                rows={3}
+                onChange={(e) => this.handleReview(e)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer className="d-flex justify-content-between">
+          <Button
+            variant={method === "PUT" ? "danger" : "secondary"}
+            onClick={
+              method === "PUT"
+                ? () => this.handleSubmitReview("DELETE")
+                : onHide
+            }
+          >
+            {method === "PUT" ? "DELETE" : "Close"}
+          </Button>
+          <Button
+            variant={method === "POST" ? "primary" : "warning"}
+            onClick={() => this.handleSubmitReview()}
+          >
+            {method === "POST" ? "Submit" : "Save changes"}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
-import { Row, Col, Card, CardColumns } from "react-bootstrap";
-import { FaStarHalf, FaStar, FaRegStar } from "react-icons/fa";
-
+import { Row, Col, Card, CardColumns, Button } from "react-bootstrap";
+import {
+  FaStarHalf,
+  FaStar,
+  FaRegStar,
+  FaThermometerHalf,
+} from "react-icons/fa";
+import AddReview from "../../components/AddReview";
+import ReviewModal from "../../components/ReviewModal";
 export default class ProductDetails extends Component {
-  state = { product: {} };
+  state = { product: {}, showModal: false, reviewId: "" };
   url = process.env.REACT_APP_API_URL;
+  productId = this.props.match.params.id;
   getSingleProduct = async () => {
     try {
-      const res = await fetch(
-        `${this.url}/products/${this.props.match.params.id}`,
-        { method: "GET", headers: { origin: process.env.ORIGIN } }
-      );
+      const res = await fetch(`${this.url}/products/${this.productId}`, {
+        method: "GET",
+        headers: { origin: process.env.ORIGIN },
+      });
       if (!res.ok) throw new Error(res);
-      const data = res.json();
-      // this.setState({ product });
+      const data = await res.json();
+      this.setState({ product: data });
     } catch (error) {
       console.log(error);
     }
@@ -27,45 +34,83 @@ export default class ProductDetails extends Component {
       stars.push(<FaRegStar style={{ color: "#grey" }} />);
     }
   };
+  showReviewModal = (e) => {
+    this.setState({
+      showModal: !this.state.showModal,
+    });
+    if (e) {
+      this.setState({ method: e.target.name, reviewId: e.target.id });
+    }
+  };
   componentDidMount = () => {
     this.getSingleProduct();
   };
   render() {
-    const { product } = this.state;
+    const { product, showModal, method, reviewId } = this.state;
     return (
       <div>
-        <Row>
-          <Col md={4}>
-            <img
-              src={product.imageUrl}
-              style={{ height: "300px", cover: "cover" }}
-            />
+        {Object.keys(product).length !== 0 && (
+          <Col lg={10} className="mx-auto my-5">
+            <Row className="my-5">
+              <Col md={4}>
+                <img
+                  src={product.imageUrl}
+                  style={{ height: "300px", cover: "cover" }}
+                />
+              </Col>
+              <Col md={8}>
+                <h4>{product.name}</h4>
+                <h5>{product.brand}</h5>
+                <hr />
+                <p>{product.descirption}</p>
+                <p>{product.price}</p>
+              </Col>
+            </Row>
+            <Row>
+              <ReviewModal
+                reviewId={reviewId}
+                method={method}
+                show={showModal}
+                onHide={() => this.showReviewModal()}
+                product={product}
+                refetch={() => this.getSingleProduct()}
+              />
+              <CardColumns>
+                {product.reviews.map((review, index) => (
+                  <Card key={`review-${index}`}>
+                    <Card.Body>
+                      <Card.Title>{review.rate} / 5</Card.Title>
+                      <Card.Text>{review.comment}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer className="d-flex row justify-content-between mx-0 py-1 px-3">
+                      <Button
+                        size="sm"
+                        variant="outline-secondary"
+                        name="PUT"
+                        id={review._id}
+                        onClick={(e) => this.showReviewModal(e)}
+                      >
+                        Edit
+                      </Button>
+                      <div className="text-muted">
+                        {new Date(review.createdAt).toLocaleDateString()}
+                      </div>
+                    </Card.Footer>
+                  </Card>
+                ))}
+              </CardColumns>
+            </Row>
+            <Col className="mx-auto">
+              <Button
+                variant="primary"
+                name="POST"
+                onClick={(e) => this.showReviewModal(e)}
+              >
+                Add review
+              </Button>
+            </Col>
           </Col>
-          <Col md={8}>
-            <h4>{product.name}</h4>
-            <h5>{product.brand}</h5>
-            <hr />
-            <p>{product.descirption}</p>
-            <p>{product.price}</p>
-          </Col>
-        </Row>
-        <Row>
-          <CardColumns>
-            {product.reviews.map((review) => (
-              <Card>
-                <Card.Body>
-                  <Card.Title>{review.rate} / 5</Card.Title>
-                  <Card.Text>{review.comment}</Card.Text>
-                  <Card.Text>
-                    <small className="text-muted">
-                      {new Date(review.createdAt).toDateString()}
-                    </small>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            ))}
-          </CardColumns>
-        </Row>
+        )}
       </div>
     );
   }
